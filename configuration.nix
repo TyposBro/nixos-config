9c52e514a5a81247f4faed56ddfc9dc12ba92df8
typosbro <typosbro@proton.me>
2024-08-15 18:56:25 +0900

    enable nvidia and swap 16GB

--- configuration.nix
+++ configuration.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
 {
   imports =
@@ -13,6 +13,13 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+ 
+  # Create a Swap
+
+  swapDevices = [{
+      device = "/swapfile";
+      size = 16 * 1024; # 16GB
+   }];
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
@@ -26,6 +33,7 @@
 
   # Set your time zone.
   time.timeZone = "Asia/Seoul";
+
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
@@ -132,30 +140,32 @@
   environment.systemPackages = with pkgs; [
    vim
    wget
-   pkgs.vscode
-   pkgs.gnome3.gnome-tweaks
-   pkgs.zsh
-   pkgs.oh-my-zsh
-   pkgs.discord
-   pkgs.spotify
-   pkgs.git
-   pkgs.android-studio   
-   pkgs.nodenv
-   pkgs.nodejs_20
-   pkgs.bun
-   pkgs.protonvpn-gui
-   pkgs.galaxy-buds-client
-   pkgs.telegram-desktop
-   pkgs.easyeffects
-   pkgs.unzip
-   pkgs.spicetify-cli
-
-
-
-
-
-
-
+   vscode
+   gnome3.gnome-tweaks
+   zsh
+   oh-my-zsh
+   discord
+   spotify
+   git
+   android-studio   
+   nodenv
+   nodejs_20
+   bun
+   protonvpn-gui
+   galaxy-buds-client
+   telegram-desktop
+   easyeffects
+   unzip
+   spicetify-cli
+   variety
+   xorg.xrandr
+   lshw
+   busybox  
+   openjdk11
+   github-desktop
+   openssl
+   android-tools   
+   xorg.xdm  
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
@@ -184,5 +194,57 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.05"; # Did you read the comment?
+
+
+
+  ############################################################################################################
+  #! NVIDIA SPECIFIC
+
+  # Enable OpenGL
+  hardware.opengl = {
+    enable = true;
+  };
+  # Load nvidia driver for Xorg and Wayland
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware.nvidia = {
+
+    # Modesetting is required.
+    modesetting.enable = true;
+
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    # Enable this if you have graphical corruption issues or application crashes after waking
+    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
+    # of just the bare essentials.
+    powerManagement.enable = false;
+
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    powerManagement.finegrained = false;
+
+    # Use the NVidia open source kernel module (not to be confused with the
+    # independent third-party "nouveau" open source driver).
+    # Support is limited to the Turing and later architectures. Full list of 
+    # supported GPUs is at: 
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+    # Only available from driver 515.43.04+
+    # Currently alpha-quality/buggy, so false is currently the recommended setting.
+    open = false;
+
+    # Enable the Nvidia settings menu,
+	# accessible via `nvidia-settings`.
+    nvidiaSettings = true;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+
+  hardware.nvidia.prime = {
+    sync.enable = true;
+		# Make sure to use the correct Bus ID values for your system!
+		intelBusId = "PCI:0:2:0";
+		nvidiaBusId = "PCI:1:0:0";
+                # amdgpuBusId = "PCI:54:0:0"; For AMD GPU
+	};
 
 }
